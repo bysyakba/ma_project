a91cf1fc3bd81dac562cfe81eb757dd8276e7c20
Egor <tyfg6594@gmail.com>
2025-01-23 00:40:53 +0400

    GENIUS++ (GOD but lazy)

--- constants.py
+++ constants.py
@@ -1,2 +1,5 @@
 VALID_EMAILS = ["example@example.com", "user@domain.co.uk", "pavel@gmail.com", "a@b.c", "user@domain.c"]
 INVALID_COUNT = [2, 11, 'a', '!', -5, 100, '4f', 2.5, True, None, '']
+NEW_VALID_EMAILS = [("pavel@gmail.com", "ivan@anal.nedorogo"), ("user@domain.c", "vany-nezhnyiy@minet.net")]
+NEW_INVALID_EMAILS = [("pavel@gmail.com", "ivananal.nedorogo"), ("user@domain.c", "vany-nezhnyiy@minet.net", "ufyuf@fyuf.uy"), ("nesosaniy@vankom.xuy", "vany-nezhnyiy@minet.net")]
+CASE_ERROR = {"pavel@gmail.com" : (1337, "Я индикатор некорректного емейла") , "user@domain.c" : 400, "nesosaniy@vankom.xuy" : 404}
--- tests/test_post.py
+++ tests/test_post.py
@@ -1,10 +1,14 @@
 import pytest
 from routes.request import request
+from routes.request_putch_email import RequestPutchEmail
 from helpers.request_helper import email_exists_in_file, email_paparser, degenerate_url, invalid_degenerate_url
-from constants import VALID_EMAILS
+from constants import VALID_EMAILS, NEW_VALID_EMAILS, NEW_INVALID_EMAILS, CASE_ERROR
 from conftest import delete_vaild_emails_file
 
-class TestRequestPost:
+class TestRequests:
+
+    d = 0
+    p = 0
 
     @pytest.mark.parametrize('url', email_paparser(degenerate_url()))
     def test_email_2valid(self, url, delete_vaild_emails_file):
@@ -24,3 +28,20 @@ class TestRequestPost:
     def test_email_invalid(self, invalid_degeneratbl):
         response = email_paparser(invalid_degeneratbl)
         assert response == 404
+
+    @pytest.mark.parametrize('emails', NEW_VALID_EMAILS)
+    def test_putch_valid(self, emails, delete_vaild_emails_file):
+        [request.post(y) for y in VALID_EMAILS]
+        response = RequestPutchEmail.patch(emails)
+        assert response == 200
+        assert email_exists_in_file(NEW_VALID_EMAILS[TestRequests.d][1])
+        assert not email_exists_in_file(NEW_VALID_EMAILS[TestRequests.d][0])
+        TestRequests.d += 1
+
+    @pytest.mark.parametrize('emails', NEW_INVALID_EMAILS)
+    def test_putch_invalid(self, emails, delete_vaild_emails_file):
+        [request.post(y) for y in VALID_EMAILS]
+        response = RequestPutchEmail.patch(emails)
+        assert response == CASE_ERROR.get(emails[0])
+        assert not email_exists_in_file(NEW_INVALID_EMAILS[TestRequests.p][1])
+        TestRequests.p += 1
