dbda68ce500c4494c873f70b66c7e2bd943ebbba
Egor <tyfg6594@gmail.com>
2025-01-22 23:49:38 +0400

    dz+hardCODE

--- constants.py
+++ constants.py
@@ -1,1 +1,2 @@
 VALID_EMAILS = ["example@example.com", "user@domain.co.uk", "pavel@gmail.com", "a@b.c", "user@domain.c"]
+INVALID_COUNT = [2, 11, 'a', '!', -5, 100, '4f', 2.5, True, None, '']
--- tests/test_post.py
+++ tests/test_post.py
@@ -1,12 +1,26 @@
 import pytest
 from routes.request import request
-from helpers.request_helper import email_exists_in_file
+from helpers.request_helper import email_exists_in_file, email_paparser, degenerate_url, invalid_degenerate_url
 from constants import VALID_EMAILS
+from conftest import delete_vaild_emails_file
 
 class TestRequestPost:
-    
-    @pytest.mark.parametrize('email', VALID_EMAILS)
-    def test_email_valid(self, email, delete_vaild_emails_file):
-        response = request.post(email)
-        assert response == 200
-        assert email_exists_in_file(email)
+
+    @pytest.mark.parametrize('url', email_paparser(degenerate_url()))
+    def test_email_2valid(self, url, delete_vaild_emails_file):
+        response = request.post(url)
+        assert response[0] == 200
+        assert email_exists_in_file(url)
+
+
+    @pytest.mark.parametrize('url', email_paparser(degenerate_url()))
+    def test_email_2valid(self, url, delete_vaild_emails_file):
+        response = request.post(url)
+        assert response[0] == 200
+        assert email_exists_in_file(url)
+
+
+    @pytest.mark.parametrize('invalid_degeneratbl', invalid_degenerate_url())
+    def test_email_invalid(self, invalid_degeneratbl):
+        response = email_paparser(invalid_degeneratbl)
+        assert response == 404
